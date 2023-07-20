ecb6ebd014f29f6eb98825331f5b4cd25ddfc2f3
Ace Gabriel P. Pasiliao <114063817+lugh-tuatha@users.noreply.github.com>
2023-07-20 16:13:57 +0800

    integrate: google sign in

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const app = express();
+const port = 9000;
+
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 
-app.listen(9002, function check(error){
-  if(error){
-    console.log("Errorrr...!!")
-  }else{
-    console.log("Started....!!!!")
-  }
-});
+// app.listen(9000, function check(error){
+//   if(error){
+//     console.log("Errorrr...!!")
+//   }else{
+//     console.log("Started....!!!!")
+//   }
+// });
 
-mongoose.connect("mongodb://localhost:27017/abc", {useNewUrlParser: true, useUnifiedTopology: true},
-function checkDb(error) {
-  if(error){
-    console.log("Error Connecting to DB");
-  }else{
-    console.log("Successfully Connected to DB");
-  }
+// mongoose.connect("mongodb://localhost:27017/abc", {useNewUrlParser: true, useUnifiedTopology: true},
+// function checkDb(error) {
+//   if(error){
+//     console.log("Error Connecting to DB");
+//   }else{
+//     console.log("Successfully Connected to DB");
+//   }
+// })
+
+app.post('/api/login', (req, res) => {
+  console.log(req.body);
+  res.redirect('http://localhost:4200/timesheet/')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
 })
