18fb09a0d525af8af66168e3c7a91c88c43c2206
Ace Gabriel P. Pasiliao <114063817+lugh-tuatha@users.noreply.github.com>
2023-08-10 09:43:00 +0800

    create: development config json and configuration

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
+const DBConfig = require('../services/shared/config-service').getConf('DB')
+
 const mongoose = require('mongoose');
 
 const DB = process.env.MONGO_URI.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
 const connectDB = async () => {
   try{
-    const conn = await mongoose.connect(DB);
+    const conn = await mongoose.connect(DB || DBConfig.DatabaseUrl);
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
   }catch (error){
---
+++ services/shared/config-service.js
@@ -0,0 +1,14 @@
+const path = `../../config/${process.env.NODE_ENV || 'development'}`
+const DBConfig = require(`${path}/DBconfig`)
+
+function getConf(conf){
+  switch(conf) {
+    case 'DB': {
+      return DBConfig
+    }
+  }
+}
+
+module.exports ={
+  getConf: getConf
+}
